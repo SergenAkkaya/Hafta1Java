82fead021a111ec760d73db569805432fd993657
SergenAkkaya <akkayasergen384@gmail.com>
2025-10-22 15:31:51 +0300

    Hafta 1 Projeler
    Yeni örnek eklendi.

--- hafta1.java
+++ hafta1.java
@@ -28,5 +28,28 @@ public class hafta1 {
         } else {
             System.out.println("Geçersiz not girdiniz.");
         }
+
+        /*boy ve kilo değerlerine göre vücut kitle indeksini hesaplayan program  bunu sınıflanrdırın
+        1. Zayıf: VKİ < 18.5
+        2. Normal: 18.5 ≤ VKİ < 24.9
+        3. Fazla Kilolu: 25 ≤ VKİ < 29.9
+        4. Obez: VKİ ≥ 30
+        */
+        double  boy = 1.75; //metre cinsinden                   
+
+        double kilo = 70; //kilogram cinsinden  
+
+        double vki = kilo / (boy * boy);
+        System.out.println("Vücut Kitle İndeksi: " + vki);  
+
+        if (vki < 20) {
+            System.out.println("Durum: çok zayıf");
+        } else if (vki < 25) {
+            System.out.println("Durum: zayıf");
+        } else if (vki < 30) {
+            System.out.println("Durum: normal");
+        } else {
+            System.out.println("Durum: Obez");
+        }
     }
 }
